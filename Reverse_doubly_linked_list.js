e357ef185b5e62c17a0aa9aadaf242406e89f51f
Yan Chan Lin <yonalin1@gmail.com>
2020-03-10 00:42:29 -0400

    Create Reverse_doubly_linked_list.js

---
+++ Reverse_doubly_linked_list.js
@@ -0,0 +1,40 @@
+/*
+Reverse a doubly linked list.
+
+The main thing to consider is that we need to swap elements on opposite ends of the linked list.
+Basically the values of each node's previous and nextNode have been swapped. 
+Start at the head, swap its prev/ next nodes. 
+*/
+
+Reverse a doubly linked list. 
+The main thing to consider is that we need to swap elements on opposite ends of the linked list.
+The values of each node's previous and nextNode have been swapped. 
+Start at the head, swap its prev/ next nodes. 
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode next;
+ *     DoublyLinkedListNode prev;
+ * }
+ * N is the number of elements in the linked list. 
+ */
+
+
+function reverse(head) {
+    let node = head;
+
+    if(!head) {return null}
+
+    while(node) {
+        let nextNode = node.next; 
+
+        node.next = node.prev;
+        node.prev = nextNode;
+        head = node; 
+        node = nextNode;
+    }   
+    return head; 
+}
