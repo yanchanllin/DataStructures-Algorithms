e62e705d0eb8031ccc92f9428a36030a73de24df
Yan Chan Lin <yonalin1@gmail.com>
2020-03-12 20:33:02 -0400

    Update reverseArray.js

--- reverseArray.js
+++ reverseArray.js
@@ -1,19 +1,19 @@
 function reverseArray(a) {
 
-      var arr = [1,2,3,4];
-      var string = '';
-for (var i=arr.length-1; i>=0; i--) {
-  string = string.concat(arr[i]) + ' '
-}
+     var arr = [1,2,3,4];
+     var string = '';
+  for (var i=arr.length-1; i>=0; i--) {
+   string = string.concat(arr[i]) + ' '
+ }
 console.log(string.trim()); 
 }
 
 //Another way:
 function reverseArray(a) {
 
-let arr = [1,2,3,4];
-let string = '';
-arr.reverse().forEach(item => string = `${string} ${item}`)
+    let arr = [1,2,3,4];
+    let string = '';
+  arr.reverse().forEach(item => string = `${string} ${item}`)
 
-console.log(string.trim());
+ console.log(string.trim());
 }
