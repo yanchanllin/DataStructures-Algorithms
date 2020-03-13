6612713c568613ccb378a4616f572084ed530a23
Yan Chan Lin <yonalin1@gmail.com>
2020-03-12 20:31:58 -0400

    Create reverseArray.js

---
+++ reverseArray.js
@@ -0,0 +1,19 @@
+function reverseArray(a) {
+
+      var arr = [1,2,3,4];
+      var string = '';
+for (var i=arr.length-1; i>=0; i--) {
+  string = string.concat(arr[i]) + ' '
+}
+console.log(string.trim()); 
+}
+
+//Another way:
+function reverseArray(a) {
+
+let arr = [1,2,3,4];
+let string = '';
+arr.reverse().forEach(item => string = `${string} ${item}`)
+
+console.log(string.trim());
+}
