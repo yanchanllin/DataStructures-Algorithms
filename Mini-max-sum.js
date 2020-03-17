cad5a0212db7da12a4044f90f09c565fbf2225cb
Yan Chan Lin <yonalin1@gmail.com>
2020-03-17 13:57:50 -0400

    Create Mini-max-sum.js

---
+++ Mini-max-sum.js
@@ -0,0 +1,12 @@
+function miniMaxSum(arr) {
+
+    let totals = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    for (let i = 0; i < arr.length; i++) {
+        let filtered = arr.filter(function(value, index, arr) {
+            return i !== index;
+        });
+        totals.push(filtered.reduce(reducer));
+    }
+    console.log(Math.min(...totals) + " " + Math.max(...totals));
+}
