90d7f8f1c88ba11dbb09dc19b9bcc536c44f946c
Yan Chan Lin <yonalin1@gmail.com>
2020-03-20 12:14:27 -0400

    Count Tallest Candle

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -3,7 +3,6 @@ find out how many candles she can successfully blow out.
 For example, if your niece is turning  years old, and the cake will have 4candles of height 4,4,1,3, 
 she will be able to blow out 2 candles successfully, since the tallest candles are of height 4 and there are 2 such candles.
 */
-
 function birthdayCakeCandles(ar) {
           let maxHeight = Math.max(...ar); 
           let count = 0; 
