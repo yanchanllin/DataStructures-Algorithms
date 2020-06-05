b12d9a799e0c0b9820b6b4561f4379a29e874bcb
Yan Chan Lin <yonalin1@gmail.com>
2020-06-05 16:32:04 -0400

    Update insert-sorted-doubly-linked-list.js

--- insert-sorted-doubly-linked-list.js
+++ insert-sorted-doubly-linked-list.js
@@ -17,7 +17,6 @@ function sortedInsert(head, data) {
 
     return head
 }
-
 /*
 Given a reference to the head of a doubly-linked list and an integer, , create a new DoublyLinkedListNode object having data value  and insert it into a sorted linked list while maintaining the sort.
 
