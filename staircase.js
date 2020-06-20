816e5f219366a71469b3c39bb4a4f009a2fb9876
Yan Chan Lin <yonalin1@gmail.com>
2020-06-20 14:04:14 -0400

    Update staircase.js

--- staircase.js
+++ staircase.js
@@ -40,7 +40,6 @@ Explanation
 
 The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n= 6.
 */
-
 function staircase(n) {
   
  for(var i = 1; i <= n; i++){
