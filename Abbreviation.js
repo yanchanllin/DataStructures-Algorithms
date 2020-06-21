28695b9a57e65e7c690b18b4ee018930e7e7d078
Yan Chan Lin <yonalin1@gmail.com>
2020-06-21 01:52:30 -0400

    Update Abbreviation.js

--- Abbreviation.js
+++ Abbreviation.js
@@ -1,10 +1,10 @@
-*/
+/*
 We have  a=daBcd and  b=ABC. We perform the following operation:
 
 Capitalize the letters a and c in  so that  dABCd
 Delete all the remaining lowercase letters in  so that  ABC.
 Because we were able to successfully convert a to b, we print YES on a new line.
-/*
+*/
 
 function abbreviation(a, b) {
 
